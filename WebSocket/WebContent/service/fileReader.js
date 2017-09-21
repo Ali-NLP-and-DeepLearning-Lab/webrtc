b4a031e6b244cf9f0b8808e16da4098926622e79
hyunjun <homelus@daum.net>
2017-09-21 15:04:58 +0900

    1. update and commit

--- WebSocket/WebContent/service/fileReader.js
+++ WebSocket/WebContent/service/fileReader.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
  self.onmessage = function (e) {
 	
 	var file = e.data;
@@ -22,47 +21,6 @@
 	}
 	
 	fileReader.onloadend = function (e) {
-=======
-self.onmessage = function (e) {
-	
-	console.log('webworker onmessage');
-	
-	var file = e.data;
-	
-	console.log(file);
-	
-	var fileReader = new FileReader();
-	
-	fileReader.onloadstart = function (e) {
-		console.log('onloadstart');
-		console.log(e);
-	}
-	
-	fileReader.onprogress = function (e) {
-		console.log('onprogress');
-		console.log(e);
-	}
-
-
-	fileReader.onload = function (e) {
-		console.log('onload');
-		console.log(e);
-	}
-	
-	fileReader.onerror = function (e) {
-		console.log('onerror');
-		console.log(e);
-	}
-	
-	fileReader.onabort = function (e) {
-		console.log('onabort');
-		console.log(e);
-	}
-	
-	fileReader.onloadend = function (e) {
-		console.log('onloaded');
-		console.log(e);
->>>>>>> branch 'master' of https://github.com/YounHyunJun/webrtc.git
 		postMessage(e.srcElement.result);
 	}
 	
